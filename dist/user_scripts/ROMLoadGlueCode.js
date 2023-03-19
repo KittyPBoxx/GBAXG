47c138338877a61193397602593bcd34f330d5c3
KittyPBoxx <kittypboxx@gmail.com>
2023-03-19 06:19:41 +0000

    Fix Last commit

--- dist/user_scripts/ROMLoadGlueCode.js
+++ dist/user_scripts/ROMLoadGlueCode.js
@@ -66,7 +66,7 @@ function fileLoadShimCode(files, ROMHandler, code) {
 }
 function fileLoadBIOS() {
     fileLoadShimCode(this.files, attachBIOS, "");
-    this.IOCore.forceBiosFile = true;
+    IodineGUI.Iodine.IOCore.forceBiosFile = true;
 }
 function fileLoadROM(files, code) {
     fileLoadShimCode(files, attachROM, code);
